6880d4ebe181091dbf6f3c878a9d4434cd3afab4
sharpyx <sckopincev.artyom@gmail.com>
2023-03-06 14:32:25 +0400

    Installed prettier and added config file for it.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-	site: "https://skopincev.dev",
-	integrations: [mdx(), sitemap()],
+  site: "https://skopincev.dev",
+  integrations: [mdx(), sitemap(), react(), tailwind()],
 });
